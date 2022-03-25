21d945bd78ebc0e18b3df975e7a436ff4786473f
Alessandra Reis <alereis1002@gmail.com>
2022-03-25 16:12:07 +0000

    current location
    identifying current location of the user

--- SolaApp/src/app/tab3/tab3.module.ts
+++ SolaApp/src/app/tab3/tab3.module.ts
@@ -7,6 +7,8 @@ import { Tab3Page } from './tab3.page';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 @NgModule({
   imports: [
     IonicModule,
@@ -15,6 +17,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page],
+  providers: [Geolocation]
 })
 export class Tab3PageModule {}
--- SolaApp/src/app/tab3/tab3.page.ts
+++ SolaApp/src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -11,10 +12,11 @@ export class Tab3Page {
   tittle: string = "Location";
 
   map: google.maps.Map;
+  myPosition: google.maps.LatLng;
 
   @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) {}
 
   ionViewWillEnter(){
     this.showMap();
@@ -24,20 +26,42 @@ export class Tab3Page {
     const position = new google.maps.LatLng(53.35833165681358, -6.257304000372879);
     const options = {
       center: position,
-      zoom: 15,
+      zoom: 1,
       disableDefaultUI: true
     }
 
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
 
-    new google.maps.Marker({
-      position: position,
-      map: this.map,
-      title: "Dorset College",
-      animation: google.maps.Animation.BOUNCE
-    });
+    this.findPosition();
+
+
   }
 
+  findPosition() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
 
+      this.myPosition = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+      this.goToMyPosition();
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+
+  goToMyPosition() {
+    this.map.setCenter(this.myPosition);
+    this.map.setZoom(15);
+
+    const marker = new google.maps.Marker({
+      position: this.myPosition,
+      title: 'Your Location!',
+      animation: google.maps.Animation.BOUNCE,
+      map: this.map
+    })
+
+  }
 
 }
