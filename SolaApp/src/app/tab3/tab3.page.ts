488a24b1b54320e65e9082ab23f2b9a7ee97c04c
Alessandra Reis <alereis1002@gmail.com>
2022-03-25 17:06:59 +0000

    Maps Update
    Added search bar

--- SolaApp/src/app/tab3/tab3.page.ts
+++ SolaApp/src/app/tab3/tab3.page.ts
@@ -1,54 +1,61 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import {Geolocation} from '@ionic-native/geolocation/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
-  tittle: string = "Location";
+  tittle: string = 'Location';
 
   map: google.maps.Map;
   myPosition: google.maps.LatLng;
 
-  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+  private autoComplete = new google.maps.places.AutocompleteService();
+
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
 
   constructor(private geolocation: Geolocation) {}
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.showMap();
   }
 
   showMap() {
-    const position = new google.maps.LatLng(53.35833165681358, -6.257304000372879);
+    const position = new google.maps.LatLng(
+      53.35833165681358,
+      -6.257304000372879
+    );
     const options = {
       center: position,
       zoom: 1,
-      disableDefaultUI: true
-    }
+      disableDefaultUI: true,
+    };
 
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
 
     this.findPosition();
-
-
   }
 
   findPosition() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      // resp.coords.latitude
-      // resp.coords.longitude
+    this.geolocation
+      .getCurrentPosition()
+      .then((resp) => {
+        // resp.coords.latitude
+        // resp.coords.longitude
 
-      this.myPosition = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+        this.myPosition = new google.maps.LatLng(
+          resp.coords.latitude,
+          resp.coords.longitude
+        );
 
-      this.goToMyPosition();
-
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });
+        this.goToMyPosition();
+      })
+      .catch((error) => {
+        console.log('Error getting location', error);
+      });
   }
 
   goToMyPosition() {
@@ -59,9 +66,19 @@ export class Tab3Page {
       position: this.myPosition,
       title: 'Your Location!',
       animation: google.maps.Animation.BOUNCE,
-      map: this.map
-    })
-
+      map: this.map,
+    });
   }
 
+  searchAddress(eventFieldSearch: any) {
+    const search = eventFieldSearch.target.value as string;
+
+    if (!search.trim().length) { return false; }
+
+    this.autoComplete.getPlacePredictions({input: search}, (arrayPlaces, status) => {
+
+      console.log(arrayPlaces);
+
+    });
+  }
 }
