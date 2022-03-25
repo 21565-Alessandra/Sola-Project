7c620349060eb5390f7fd3b8093433d7f4805d5e
Alessandra Reis <alereis1002@gmail.com>
2022-03-25 15:00:07 +0000

    Maps + API
    Adding maps to the app and creating API

--- SolaApp/src/app/tab3/tab3.page.ts
+++ SolaApp/src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -10,6 +10,34 @@ export class Tab3Page {
 
   tittle: string = "Location";
 
+  map: google.maps.Map;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
   constructor() {}
+
+  ionViewWillEnter(){
+    this.showMap();
+  }
+
+  showMap() {
+    const position = new google.maps.LatLng(53.35833165681358, -6.257304000372879);
+    const options = {
+      center: position,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+
+    new google.maps.Marker({
+      position: position,
+      map: this.map,
+      title: "Dorset College",
+      animation: google.maps.Animation.BOUNCE
+    });
+  }
+
+
 
 }
