eaa51d15892bf1f78264a28f8da57fc6fd3023ce
Alessandra Reis <alereis1002@gmail.com>
2022-03-28 19:03:57 +0100

    adding pages
    chat and connection pages

--- SolaApp/src/app/app-routing.module.ts
+++ SolaApp/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { ChatPage } from './chat/chat.page';
 import { Tab1Page } from './tab1/tab1.page';
 import { Tab2Page } from './tab2/tab2.page';
 import { Tab3Page } from './tab3/tab3.page';
+import { ConnectionsPage } from './connections/connections.page';
 
 const routes: Routes = [
 
   {path: 'tabs', component: AppComponent},
 
   {path: 'chat', component: ChatPage},
+  {path: 'connections', component: ConnectionsPage},
+
   {path: 'tab1', component: Tab1Page},
   {path: 'tab2', component: Tab2Page},
   {path: 'tab3', component: Tab3Page},
@@ -33,6 +36,14 @@ const routes: Routes = [
   {
     path: 'chat',
     loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'connections',
+    loadChildren: () => import('./connections/connections.module').then( m => m.ConnectionsPageModule)
+  },
+  {
+    path: 'view-profile',
+    loadChildren: () => import('./view-profile/view-profile.module').then( m => m.ViewProfilePageModule)
   }
 ];
 @NgModule({
